1bbe52849bf08a6eda04fc186ce8f5dd5b4f01c9
Azambek Alimbaev <aalimbaev@kpfu.ru>
2026-06-19 09:56:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -19,40 +19,32 @@ def get_usd_tjs_rate():
         data = json.loads(response.read())
     return data['rates']['TJS']
 
-@app.route('/colorize', methods=['POST'])
-def colorize():
-    rate = get_usd_tjs_rate()
-    limit = 200 * rate
-
-
 @app.route('/save-url', methods=['POST'])
 def save_url():
-    data = request.json
-    resume_url_store['url'] = data['url']
+    resume_url_store['url'] = request.json['url']
     return jsonify({'ok': True})
 
 @app.route('/get-url')
 def get_url():
     return jsonify({'url': resume_url_store.get('url', '')})
 
+@app.route('/colorize', methods=['POST'])
+def colorize():
+    rate = get_usd_tjs_rate()
+    limit = 200 * rate
     file = request.files['file']
     wb = openpyxl.load_workbook(file)
     ws = wb.active
-    
-
     header_row = [cell.value for cell in ws[1]]
     color_col = None
     sum_col = None
     name_col = None
     phone_col = None
-
     for i, h in enumerate(header_row):
         if h == '_rowColor': color_col = i + 1
         if h == 'Сумма': sum_col = i + 1
         if h == 'Имя получателя физ. лица': name_col = i + 1
         if h == 'Контактный номер': phone_col = i + 1
-
-    # Группируем по получателю и считаем суммы
     groups = {}
     rows_data = []
     for row in ws.iter_rows(min_row=2):
@@ -65,13 +57,10 @@ def get_url():
             groups[key] = 0
         groups[key] += (amount or 0)
         rows_data.append({'row': row, 'key': key, 'color': color})
-
-    # Красим строки
     for item in rows_data:
         row = item['row']
         color = item['color']
         key = item['key']
-
         if color == 'red':
             for cell in row:
                 if color_col and cell.column != color_col:
@@ -80,15 +69,11 @@ def get_url():
             for cell in row:
                 if color_col and cell.column != color_col:
                     cell.fill = YELLOW_FILL
-
-    # Удаляем колонку _rowColor
     if color_col:
         ws.delete_cols(color_col)
-
     output = io.BytesIO()
     wb.save(output)
     output.seek(0)
-
     return send_file(
         output,
         mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
